f87c4880fb4f45116f12fbea94ccc57493acaacf
Niklas Bettgen <niklas@bettgen.de>
2022-05-11 09:28:11 +0200

    Recursive method to find all card permutations

--- main.py
+++ main.py
@@ -19,17 +19,35 @@ CARDS: List[Card] = [
 ]
 
 
-def find_combinations(cards: List[Card], num: int) -> List[List[Card]]:
-    """Return all possible permutations using n cards."""
-    return [cards[1:3], cards[5:7]]
+def find_combinations(cards: List[Card], n: int) -> List[List[Card]]:
+    """Recursively return all possible permutations using n cards."""
+
+    # Exit condition or parameter error
+    if n <= 0 or n > len(cards):
+        return []
+
+    # Only one level left - return each remaining card as option
+    if n == 1:
+        return [[c] for c in cards]
+
+    # More iterations necessary - extend result list by each subset
+    result: List[List[Card]] = []
+    for i in range(len(cards)):
+        remaining = cards[i + 1:]
+        subtree = find_combinations(remaining, n - 1)
+        result.extend([[cards[i], *sub] for sub in subtree])
+    return result
 
 
 def main():
     """Main entry point."""
     for num in range(1, 6):
-        print(f"=== Finding best combinations for {num} cards. ===")
-        combinations: List[Card] = find_combinations(CARDS, num)
-        print(" + ".join(c.name for c in combinations))
+        i: int = 0
+        print(f"=== Finding best combinations for {num} card(s). ===")
+        combinations: List[List[Card]] = find_combinations(CARDS, num)
+        for comb in combinations:
+            print(i, "+".join(card.name for card in comb))
+            i += 1
         print()
 
 
