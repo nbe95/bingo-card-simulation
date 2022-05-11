ad03af816889ad0fd4ab6e4c8e13551b12950ff9
Niklas Bettgen <niklas@bettgen.de>
2022-05-11 09:35:42 +0200

    Better naming

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ def find_combinations(cards: List[Card], n: int) -> List[List[Card]]:
 
     # Only one level left - return each remaining card as option
     if n == 1:
-        return [[c] for c in cards]
+        return [[x] for x in cards]
 
     # More iterations necessary - extend result list by each subset
     result: List[List[Card]] = []
     for i in range(len(cards)):
         remaining = cards[i + 1:]
         subtree = find_combinations(remaining, n - 1)
-        result.extend([[cards[i], *sub] for sub in subtree])
+        result.extend([[cards[i], *x] for x in subtree])
     return result
 
 
@@ -33,7 +33,7 @@ def main():
         print(f"=== Finding best combinations for {num} card(s). ===")
         combinations: List[List[Card]] = find_combinations(CARDS, num)
         for comb in combinations:
-            print(i, "+".join(card.get_name() for card in comb))
+            print(i, "+".join(x.get_name() for x in comb))
             i += 1
         print()
 
