cd642b668a14d5ef9ff2905658c6d1e434de4d54
Niklas Bettgen <niklas.bettgen@bertrandt-development.com>
2022-05-12 10:08:02 +0200

    Readable card definition format

--- cards.py
+++ cards.py
@@ -32,14 +32,14 @@ class Card:
 
 
 CARDS: List[Card] = [
-    Card("Red", Color("#cc0044"), [11, 27, 1, 24, 14, 18, 21, 4, 9]),
-    Card("Pink", Color("#ff88cc"), [29, 19, 13, 6, 10, 22, 3, 15, 28]),
-    Card("Light blue", Color("#0088ff"), [17, 30, 20, 7, 1, 12, 25, 8, 29]),
-    Card("Light yellow", Color("#ffff80"), [14, 8, 2, 23, 11, 25, 4, 29, 17]),
-    Card("Green", Color("#008800"), [16, 2, 19, 28, 9, 23, 12, 7, 30]),
-    Card("Purple", Color("#880088"), [12, 24, 3, 5, 16, 21, 20, 27, 8]),
-    Card("Yellow", Color("#cccc00"), [26, 22, 7, 1, 17, 27, 13, 20, 10]),
-    Card("Gray", Color("#888888"), [9, 14, 6, 22, 4, 15, 30, 18, 26]),
-    Card("Blue", Color("#00ffff"), [15, 3, 23, 25, 6, 11, 18, 28, 5]),
-    Card("Orange", Color("#ffaa00"), [5, 16, 19, 21, 2, 13, 26, 10, 24])
+    Card("Red",             Color("#cc0044"), [11, 27,  1, 24, 14, 18, 21,  4,  9]),    # noqa
+    Card("Pink",            Color("#ff88cc"), [29, 19, 13,  6, 10, 22,  3, 15, 28]),    # noqa
+    Card("Light blue",      Color("#0088ff"), [17, 30, 20,  7,  1, 12, 25,  8, 29]),    # noqa
+    Card("Light yellow",    Color("#ffff80"), [14,  8,  2, 23, 11, 25,  4, 29, 17]),    # noqa
+    Card("Green",           Color("#008800"), [16,  2, 19, 28,  9, 23, 12,  7, 30]),    # noqa
+    Card("Purple",          Color("#880088"), [12, 24,  3,  5, 16, 21, 20, 27,  8]),    # noqa
+    Card("Yellow",          Color("#cccc00"), [26, 22,  7,  1, 17, 27, 13, 20, 10]),    # noqa
+    Card("Gray",            Color("#888888"), [ 9, 14,  6, 22,  4, 15, 30, 18, 26]),    # noqa
+    Card("Blue",            Color("#00ffff"), [15,  3, 23, 25,  6, 11, 18, 28,  5]),    # noqa
+    Card("Orange",          Color("#ffaa00"), [ 5, 16, 19, 21,  2, 13, 26, 10, 24])     # noqa
 ]
