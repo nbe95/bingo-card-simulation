aa30689226d1521ba8fd17059828fd512edd8e9d
Niklas Bettgen <niklas.bettgen@bertrandt-development.com>
2022-05-12 07:24:45 +0200

    Do not create and store Card objects for every permutation
    Instead, create a huge list of int-lists, each representing an array
    index. Thus, only the global Card object list must be preserved.

--- main.py
+++ main.py
@@ -3,26 +3,27 @@
 """Main Bingo module. Which combination of cards has the highest chance to win?"""
 
 from typing import List
-from cards import Card, CARDS
+from cards import CARDS
 
 
-def find_combinations(cards: List[Card], n: int) -> List[List[Card]]:
-    """Recursively return all possible permutations using n cards."""
+def find_permutations(elements: List[int], n: int) -> List[List[int]]:
+    """Recursively return all possible permutations (without repetitions) using a set of n integers
+    representing arbitrary elements e.g. indices."""
 
-    # Exit condition or parameter error
-    if n <= 0 or n > len(cards):
+    # Exit condition in case of parameter error
+    if n <= 0 or n > len(elements):
         return []
 
-    # Only one level left - return each remaining card as option
+    # Only one level left - return each remaining element as option
     if n == 1:
-        return [[x] for x in cards]
+        return [[x] for x in elements]
 
     # More iterations necessary - extend result list by each subset
-    result: List[List[Card]] = []
-    for i in range(len(cards)):
-        remaining = cards[i + 1:]
-        subtree = find_combinations(remaining, n - 1)
-        result.extend([[cards[i], *x] for x in subtree])
+    result: List[List[int]] = []
+    for i in range(len(elements)):
+        remaining = elements[i + 1:]
+        subtree = find_permutations(remaining, n - 1)
+        result.extend([[elements[i], *x] for x in subtree])
     return result
 
 
@@ -31,9 +32,10 @@ def main():
     for num in range(1, 6):
         i: int = 0
         print(f"=== Finding best combinations for {num} card(s). ===")
-        combinations: List[List[Card]] = find_combinations(CARDS, num)
+        combinations: List[List[int]] = find_permutations(range(len(CARDS)), num)
         for comb in combinations:
-            print(i, " ".join(f"{x.draw_terminal_color()}{x.get_name():8}" for x in comb))
+            cards = [CARDS[x] for x in comb]
+            print(i, " ".join(f"{x.draw_terminal_color()}{x.get_name():8}" for x in cards))
             i += 1
         print()
 
