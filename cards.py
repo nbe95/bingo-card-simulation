ecf1893afcb9ed88a21cb52d429260d266fc6158
Niklas Bettgen <niklas.bettgen@bertrandt-development.com>
2022-05-14 10:19:02 +0200

    Add method to find the latest drawed number of a card

--- cards.py
+++ cards.py
@@ -2,28 +2,22 @@
 
 """Bingo Card module."""
 
-from typing import List
+from typing import List, Tuple
 from colour import Color  # type: ignore
 
 
 class Card:
     """Class for each Bingo card."""
-    def __init__(self, name: str, color: Color, numbers: List[int]) -> None:
+    def __init__(self, name: str, color: Color, numbers: Tuple[int, ...]) -> None:
         self.name: str = name
         self.color: Color = color
-        self.numbers: List[int] = numbers
+        self.numbers: Tuple[int, ...] = numbers
 
-    def get_name(self) -> str:
-        """Return the card's name."""
-        return self.name
-
-    def get_color(self) -> Color:
-        """Return the card's color."""
-        return self.color
-
-    def get_numbers(self) -> List[int]:
-        """Return the card's fields."""
-        return self.numbers
+    def get_pos_of_last_number(self, card_deck: Tuple[int, ...]) -> int:
+        """Get the position of this card's last drawn number in a given card set.
+        This is the total number of iterations this card needs to be completely solved."""
+        position_in_deck: Tuple[int, ...] = tuple(card_deck.index(x) for x in self.numbers)
+        return max(position_in_deck)
 
     def draw_terminal_color(self) -> str:
         """Return a color representing field of spaces for ANSI terminals."""
@@ -32,14 +26,14 @@ class Card:
 
 
 CARDS: List[Card] = [
-    Card("Red",             Color("#cc0044"), [11, 27,  1, 24, 14, 18, 21,  4,  9]),    # noqa
-    Card("Pink",            Color("#ff88cc"), [29, 19, 13,  6, 10, 22,  3, 15, 28]),    # noqa
-    Card("Light blue",      Color("#0088ff"), [17, 30, 20,  7,  1, 12, 25,  8, 29]),    # noqa
-    Card("Light yellow",    Color("#ffff80"), [14,  8,  2, 23, 11, 25,  4, 29, 17]),    # noqa
-    Card("Green",           Color("#008800"), [16,  2, 19, 28,  9, 23, 12,  7, 30]),    # noqa
-    Card("Purple",          Color("#880088"), [12, 24,  3,  5, 16, 21, 20, 27,  8]),    # noqa
-    Card("Yellow",          Color("#cccc00"), [26, 22,  7,  1, 17, 27, 13, 20, 10]),    # noqa
-    Card("Gray",            Color("#888888"), [ 9, 14,  6, 22,  4, 15, 30, 18, 26]),    # noqa
-    Card("Blue",            Color("#00ffff"), [15,  3, 23, 25,  6, 11, 18, 28,  5]),    # noqa
-    Card("Orange",          Color("#ffaa00"), [ 5, 16, 19, 21,  2, 13, 26, 10, 24])     # noqa
+    Card("Red",             Color("#cc0044"), (11, 27,  1, 24, 14, 18, 21,  4,  9)),    # noqa
+    Card("Pink",            Color("#ff88cc"), (29, 19, 13,  6, 10, 22,  3, 15, 28)),    # noqa
+    Card("Light blue",      Color("#0088ff"), (17, 30, 20,  7,  1, 12, 25,  8, 29)),    # noqa
+    Card("Light yellow",    Color("#ffff80"), (14,  8,  2, 23, 11, 25,  4, 29, 17)),    # noqa
+    Card("Green",           Color("#008800"), (16,  2, 19, 28,  9, 23, 12,  7, 30)),    # noqa
+    Card("Purple",          Color("#880088"), (12, 24,  3,  5, 16, 21, 20, 27,  8)),    # noqa
+    Card("Yellow",          Color("#cccc00"), (26, 22,  7,  1, 17, 27, 13, 20, 10)),    # noqa
+    Card("Gray",            Color("#888888"), ( 9, 14,  6, 22,  4, 15, 30, 18, 26)),    # noqa
+    Card("Blue",            Color("#00ffff"), (15,  3, 23, 25,  6, 11, 18, 28,  5)),    # noqa
+    Card("Orange",          Color("#ffaa00"), ( 5, 16, 19, 21,  2, 13, 26, 10, 24))     # noqa
 ]
